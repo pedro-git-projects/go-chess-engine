f9bd28dea2096495c20c0befa032722f4bd8051d
pedro-git-projects <pedro.generic.email@gmail.com>
2023-03-16 14:32:19 -0300

    bishop unit tests

--- src/piece/bishop_test.go
+++ src/piece/bishop_test.go
@@ -5,10 +5,10 @@ import (
 	"testing"
 
 	"github.com/pedro-git-projects/go-chess/src/board"
+	"github.com/pedro-git-projects/go-chess/src/piece"
 	"github.com/pedro-git-projects/go-chess/src/utils"
 )
 
-// TODO: write more tests to check legal moves
 func TestBishopCalculateLegalMoves(t *testing.T) {
 	b := board.New()
 	bishop := b.PieceAt(utils.NewCoordinate('c', 1))
@@ -18,9 +18,23 @@ func TestBishopCalculateLegalMoves(t *testing.T) {
 	if !reflect.DeepEqual(got, expect) {
 		t.Errorf("expected empty but got %v", got)
 	}
+
+	b.MovePiece(utils.NewCoordinate('d', 2), utils.NewCoordinate('d', 4))
+	bishop.CalculateLegalMoves(b)
+	got = bishop.LegalMoves()
+	expect = []utils.Coordinate{
+		utils.NewCoordinate('d', 2),
+		utils.NewCoordinate('e', 3),
+		utils.NewCoordinate('f', 4),
+		utils.NewCoordinate('g', 5),
+		utils.NewCoordinate('h', 6),
+	}
+
+	if !reflect.DeepEqual(got, expect) {
+		t.Errorf("expected %v but got %v", expect, got)
+	}
 }
 
-// TODO: actually caputre a piece
 func TestBishopCapture(t *testing.T) {
 	b := board.New()
 	b.MovePiece(utils.NewCoordinate('d', 2), utils.NewCoordinate('d', 4))
@@ -41,4 +55,12 @@ func TestBishopCapture(t *testing.T) {
 	if !reflect.DeepEqual(got, expect) {
 		t.Errorf("expected %v but got %v\n", expect, got)
 	}
+
+	b.MovePiece(utils.NewCoordinate('g', 8), utils.NewCoordinate('g', 5))
+	b.MovePiece(utils.NewCoordinate('d', 2), utils.NewCoordinate('g', 5))
+	p := b.PieceAt(utils.NewCoordinate('g', 5))
+	if _, ok := p.(*piece.Bishop); !ok {
+		t.Errorf("expected bishop, but got %T", reflect.TypeOf(p))
+	}
+
 }
